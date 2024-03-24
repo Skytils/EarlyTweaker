9c637cf536f57b26bf168a948dab940de5242a4d
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2024-03-24 13:34:45 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2024 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+plugins {
+    id("net.kyori.blossom") version "2.1.0"
+    id("gg.essential.loom") version "1.3.12"
+    id("gg.essential.defaults") version "0.3.0"
+    idea
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
+}
+
+version = "0.1.0"
+group = "gg.skytils"
+
+loom {
+    silentMojangMappingsLicense()
+    runConfigs {
+        getByName("client") {
+            isIdeConfigGenerated = true
+        }
+        remove(getByName("server"))
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.spongepowered.org/repository/maven-public/")
+    maven("https://repo.sk1er.club/repository/maven-public/")
+    maven("https://repo.sk1er.club/repository/maven-releases/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+
+}
+
+sourceSets {
+    main {
+        output.setResourcesDir(layout.buildDirectory.file("/classes/kotlin/main"))
+        blossom {
+            javaSources {
+                property("version", project.version.toString())
+            }
+            resources {
+                property("version", project.version.toString())
+                property("mcversion", "1.8.9")
+            }
+        }
+    }
+
+}
+
+tasks {
+    processResources {
+        dependsOn(compileJava)
+    }
+    named<Jar>("jar") {
+        archiveBaseName.set("EarlyTweaker")
+
+        manifest {
+            attributes(
+                mapOf(
+                    "Implementation-Version" to version,
+                    "ModSide" to "CLIENT",
+                    "ModType" to "FML",
+                )
+            )
+        }
+
+        enabled = false
+    }
+    withType<AbstractArchiveTask> {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release.set(8)
+    }
+}
